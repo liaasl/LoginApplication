7bec3f8a712915c446ed314fb626ff4739d88de3
liaasl <lianiaslam@gmail.com>
2023-09-30 17:44:19 +0800

    Login functionality with hardcoded data

--- login-app/src/app/login/login.component.ts
+++ login-app/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+  errorMessage: string | undefined;
+  hide = true;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      userId: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    const formValue = this.loginForm.value;
+    if (formValue.userId === 'user1' && formValue.password === 'abc123') {
+      console.log('Login Success');
+      // navigate to welcome page
+    } else {
+      this.errorMessage = 'Invalid userId or password';
+      console.log('Login Failed');
+    }
+  }
 
 }
