6f0ba1fd46bdc8a4656ee815e045c50ee22c4718
liaasl <lianiaslam@gmail.com>
2023-09-30 17:02:00 +0800

    Login page layout

--- login-app/src/app/app.module.ts
+++ login-app/src/app/app.module.ts
@@ -4,7 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 @NgModule({
   declarations: [
@@ -17,7 +28,15 @@ import { RouterModule } from '@angular/router';
     RouterModule.forRoot([
       {path: '', pathMatch: 'full', redirectTo: 'login'},
       {path: 'login', component: LoginComponent}
-    ])
+    ]),
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
