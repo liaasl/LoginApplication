d7548137788496ccf6b520a132687db2e08a2614
liaasl <lianiaslam@gmail.com>
2023-10-02 12:25:08 +0800

    Route guard to control access to pages

--- login-app/src/app/app-routing.module.ts
+++ login-app/src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'welcome', component: WelcomeComponent}
+  {path: 'welcome', component: WelcomeComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
---
+++ login-app/src/app/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core'
+import { CanActivate, Router } from '@angular/router';
+import { StorageService } from './storage.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+    
+    constructor(private storageService: StorageService, private router: Router) {}
+
+    canActivate(): boolean {
+      const isLoggedIn = this.storageService.isLoggedIn();
+      if (!isLoggedIn) {
+        this.router.navigate(['/login']);
+        return false;
+      }
+      return true;
+    }
+}
